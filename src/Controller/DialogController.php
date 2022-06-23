d7747f20890e1f8c2a8a1b5c4491c18a96310b5f
Pain_bls <pierre.1500@gmail.com>
2022-06-23 14:57:17 +0200

    Commit du 23/06/2022 Update Formation Dev Web

--- src/Controller/DialogController.php
+++ src/Controller/DialogController.php
@@ -4,19 +4,21 @@ namespace App\Controller;
 
 use App\Services\TrollService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DialogController extends AbstractController
 {
     public function defaultAction(
         Request      $request,
         TrollService $trollService
-    ): JsonResponse
+    ): Response
     {
-        $nomDuBobby = $request->get('bobby', null);
-        return $this->json([
-            'message' => $trollService->getTroll($nomDuBobby),
-        ]);
+        {
+            $nomDuBobby = $request->get('bobby', null);
+            return $this->render('index.html.twig', [
+                'troll' => $trollService->getTroll($nomDuBobby),
+            ]);
+        }
     }
 }
